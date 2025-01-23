8fa1d5268b182fb460f7e1076a5be63e21381232
BisalDangol <message2bishal@outlook.com>
2025-01-23 14:22:38 +0545

    fixed error

--- web/web/urls.py
+++ web/web/urls.py
@@ -20,7 +20,7 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(' ', views.homePage),  
+    path('', views.homePage),  
     path('about/', views.aboutPage),
     path('auth/', include('authentication.urls')), 
 ]
