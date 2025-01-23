3e876b98c95ec372313b192a0dafbfa49f426e25
BisalDangol <message2bishal@outlook.com>
2025-01-23 09:25:02 +0545

    created templates folder and link html to views.py.

--- web/web/views.py
+++ web/web/views.py
@@ -1,7 +1,10 @@
-from django.http import HttpResponse
+#from django.http import HttpResponse
+from django.shortcuts import render
 
 def homePage(request):
-    return HttpResponse("Hi, this is home page of the app")
+#    return HttpResponse("Hi, this is home page of the app")
+    return render(request, 'home.html')
 
 def aboutPage(request):
-    return HttpResponse("This is about page")
+#    return HttpResponse("This is about page")
+    return render(request,'about.html')
