8bad71c3f12fcc4227510af4a16f244f05cea65b
BisalDangol <message2bishal@outlook.com>
2025-01-23 14:19:07 +0545

    creating app called authentication

--- web/authentication/apps.py
+++ web/authentication/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AuthentationConfig(AppConfig):
+class AuthenticationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'authentation'
+    name = 'authentication'
--- web/authentication/views.py
+++ web/authentication/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def logins(request):
-    return render(request,"")
+    return render(request,'login.html')
