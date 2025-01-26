112c37509e29b3e75fd2cc81486e6863da70b39a
BisalDangol <message2bishal@outlook.com>
2025-01-26 09:43:57 +0545

    migrating models and updating the model files

--- web/authentication/models.py
+++ web/authentication/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Post (models.Model):
+class post (models.Model):
     title = models.CharField(max_length=100)
     body = models.TextField()
     slug = models.SlugField()
