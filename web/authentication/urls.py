c2ab4a34aa0a48a196377cfdaa451c1d425f8031
BisalDangol <message2bishal@outlook.com>
2025-01-27 11:54:41 +0545

    error found after make page for each post

--- web/authentication/urls.py
+++ web/authentication/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
 
+appName = 'posts'
+# app_name = 'postPage'
 
 urlpatterns = [
     path('',views.post, name="posts"),
+    path('posts/<slug:slug>/', views.postPage, name='pages'),
+    
 ]
--- web/authentication/views.py
+++ web/authentication/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from .models import Post
+from django.http import HttpResponse
 
 # Create your views here.
 def post(request):
     posts = Post.objects.all().order_by('-date')
     return render(request,'authentications/post.html', { 'posts':posts})
+
+
+def postPage(request, slug):
+    return HttpResponse(slug)
