27468bb3033acdf52f13eb1a7fc8c337e3eeee98
BisalDangol <message2bishal@outlook.com>
2025-01-27 10:41:02 +0545

    making dynamic post menu in navbar

--- web/authentication/urls.py
+++ web/authentication/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 
 urlpatterns = [
-    path('',views.post),
+    path('',views.post, name="posts"),
 ]
--- web/authentication/views.py
+++ web/authentication/views.py
@@ -3,5 +3,5 @@ from .models import Post
 
 # Create your views here.
 def post(request):
-    posts = Post.objects.all()
+    posts = Post.objects.all().order_by('-date')
     return render(request,'authentications/post.html', { 'posts':posts})
